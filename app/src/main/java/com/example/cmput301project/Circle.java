f0cdf7ad15bb1f75770573fc041144336460e57f
wilsontruong <90410232+wilsontruong@users.noreply.github.com>
2023-10-03 18:50:34 -0600

    Color for Circle.java

--- app/src/main/java/com/example/cmput301project/Circle.java
+++ app/src/main/java/com/example/cmput301project/Circle.java
@@ -3,6 +3,7 @@ package com.example.cmput301project;
 public class Circle extends Shape {
     private int x;
     private int y;
+    String color = "blue";
 
     public Circle (int x, int y) {
         this.x = x;
