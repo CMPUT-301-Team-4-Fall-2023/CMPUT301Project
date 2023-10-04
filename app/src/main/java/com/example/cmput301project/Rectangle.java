1d1b4e76bab28e4d9c5f0c403ba9f0a397fc69e4
Alex Filipescu <alexcata.filipescu@yahoo.ca>
2023-10-03 18:44:22 -0600

    added color

--- app/src/main/java/com/example/cmput301project/Rectangle.java
+++ app/src/main/java/com/example/cmput301project/Rectangle.java
@@ -3,9 +3,11 @@ package com.example.cmput301project;
 public class Rectangle extends Shape{
     private int x;
     private int y;
+    private String color = "blue";
 
-    public Rectangle(int x, int y){
+    public Rectangle(int x, int y, String color){
         this.x = x;
         this. y = y;
+        this.color = color;
     }
 }
