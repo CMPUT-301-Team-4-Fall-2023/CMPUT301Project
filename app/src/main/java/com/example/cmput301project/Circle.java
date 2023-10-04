f0ccf0b4cd071ac0844c7d0f3da76ab28af8a8c7
wilsontruong <90410232+wilsontruong@users.noreply.github.com>
2023-10-03 19:43:31 -0600

    Update Circle.java

--- app/src/main/java/com/example/cmput301project/Circle.java
+++ app/src/main/java/com/example/cmput301project/Circle.java
@@ -3,7 +3,6 @@ package com.example.cmput301project;
 public class Circle extends Shape {
     private int x;
     private int y;
-    String color = "blue";
 
     public Circle (int x, int y) {
         this.x = x;
