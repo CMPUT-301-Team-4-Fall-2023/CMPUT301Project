ec967fd74eb1e1eb67bc8d6a6b71631c7fe102bf
Eric Xiong <90705758+ericxiong1@users.noreply.github.com>
2023-10-10 14:29:09 -0600

    resolve conflict

--- app/src/main/java/com/example/cmput301project/Shape.java
+++ app/src/main/java/com/example/cmput301project/Shape.java
@@ -3,6 +3,6 @@ package com.example.cmput301project;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "Yellow";
+    private String color = "blue";
 
 }
