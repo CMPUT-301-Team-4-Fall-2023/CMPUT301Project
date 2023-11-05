61f5159f93253f947fbceed03488e85a14747104
Pubstare <90579602+Pubstare@users.noreply.github.com>
2023-11-04 19:26:38 -0600

    Add files via upload
    ill fix it

---
+++ app/src/main/Photograph.java
@@ -0,0 +1,17 @@
+package com.example.cmput301project;
+
+public class Photograph {
+    private String path;
+
+    public Photograph(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
