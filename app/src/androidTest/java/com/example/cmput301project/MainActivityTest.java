d3ee115c7f3bc779ed32b7afaaff1a2af6dfa3c7
Eric Xiong <exiong@ualberta.ca>
2023-12-04 15:33:26 -0700

    photo deletion, edit photo addition

--- app/src/androidTest/java/com/example/cmput301project/MainActivityTest.java
+++ app/src/androidTest/java/com/example/cmput301project/MainActivityTest.java
@@ -17,6 +17,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static java.lang.Thread.sleep;
 
+import android.app.Dialog;
 import android.view.View;
 
 import androidx.test.core.app.ActivityScenario;
@@ -33,6 +34,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -46,138 +48,163 @@ public class MainActivityTest {
     public ActivityScenarioRule<LoginActivity> activityRule = new ActivityScenarioRule<>(LoginActivity.class);
     @Mock
     public FirebaseUser user;
+
+    public boolean setUp = false;
+
+    @Test
+    public void loginSetup(){
+        Intents.init();
+        onView(withId(R.id.emailEntry))
+                .perform(ViewActions.typeText("e@e.com"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.passwordEntry))
+                .perform(ViewActions.typeText("123321"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.signInButton)).perform(click());
+        try{
+            // Espresso is so bad that without this sleep the test fails, even though visually
+            // it still works
+            Thread.sleep(2000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        intended(hasComponent(MainActivity.class.getName()));
+        Intents.release();
+    }
     @Test
     public void testAddItem() {
-        Intents.init();
-        onView(withId(R.id.emailEntry))
-                .perform(ViewActions.typeText("admin@admin.com"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.passwordEntry))
-                .perform(ViewActions.typeText("password"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.signInButton)).perform(click());
-        try{
-            // Espresso is so bad that without this sleep the test fails, even though visually
-            // it still works
-            Thread.sleep(3000);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        intended(hasComponent(MainActivity.class.getName()));
-        Intents.release();
         onView(withId(R.id.add_item_button)).perform(click());
+        onView(withText("OK")).perform(click());
         onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House"));
         onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("My House"));
         onView(withId(R.id.serial_edit_text)).perform(ViewActions.typeText("12345"));
         onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("House Model"));
         onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("House Make"));
-        onView(withId(R.id.item_day_edit_text)).perform(ViewActions.typeText("12"));
-        onView(withId(R.id.item_month_edit_text)).perform(ViewActions.typeText("06"));
-        onView(withId(R.id.item_year_edit_text)).perform(ViewActions.typeText("2023"));
+        onView(withText("MM/DD/YYYY")).perform(click());
+        onView(withText("OK")).perform(click());
         onView(withId(R.id.input_tag_edit_text)).perform(ViewActions.typeText("My Tag"));
         onView(withId(R.id.add_tags_button)).perform(click());
         onView(withId(R.id.price_edit_text)).perform(ViewActions.typeText("1234.56"));
         onView(withId(R.id.comments_edit_text)).perform(ViewActions.typeText("Some comment"));
-
-        //TODO: dialog button has no id, so I cannot exit the dialog properly
-    }
-    public void testViewItem() {
-        Intents.init();
-        onView(withId(R.id.emailEntry))
-                .perform(ViewActions.typeText("admin@admin.com"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.passwordEntry))
-                .perform(ViewActions.typeText("password"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.signInButton)).perform(click());
-        try{
-            // Espresso is so bad that without this sleep the test fails, even though visually
-            // it still works
-            Thread.sleep(3000);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        intended(hasComponent(MainActivity.class.getName()));
-        Intents.release();
-        onView(withId(R.id.add_item_button)).perform(click());
-        onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House"));
-        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("My House"));
-        onView(withId(R.id.serial_edit_text)).perform(ViewActions.typeText("12345"));
-        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("House Model"));
-        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("House Make"));
-        onView(withId(R.id.item_day_edit_text)).perform(ViewActions.typeText("12"));
-        onView(withId(R.id.item_month_edit_text)).perform(ViewActions.typeText("06"));
-        onView(withId(R.id.item_year_edit_text)).perform(ViewActions.typeText("2023"));
-        onView(withId(R.id.input_tag_edit_text)).perform(ViewActions.typeText("My Tag"));
-        onView(withId(R.id.add_tags_button)).perform(click());
-        onView(withId(R.id.price_edit_text)).perform(ViewActions.typeText("1234.56"));
-        onView(withId(R.id.comments_edit_text)).perform(ViewActions.typeText("Some comment"));
-
-        //TODO: dialog button has no id, so I cannot exit the dialog properly
-        onView(withId(R.id.view_item_button)).perform(click());
+        onView(withText("OK")).perform(click());
         onView(withText("House")).check(matches(isDisplayed()));
 
     }
-    public void testDeleteItem() {
-        Intents.init();
-        onView(withId(R.id.emailEntry))
-                .perform(ViewActions.typeText("admin@admin.com"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.passwordEntry))
-                .perform(ViewActions.typeText("password"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.signInButton)).perform(click());
-        try{
-            // Espresso is so bad that without this sleep the test fails, even though visually
-            // it still works
-            Thread.sleep(3000);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        intended(hasComponent(MainActivity.class.getName()));
-        Intents.release();
+    @Test
+    public void testFilterItem() {
         onView(withId(R.id.add_item_button)).perform(click());
-        onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House"));
-        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("My House"));
+        onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House1"));
+        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("House1"));
         onView(withId(R.id.serial_edit_text)).perform(ViewActions.typeText("12345"));
         onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("House Model"));
         onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("House Make"));
-        onView(withId(R.id.item_day_edit_text)).perform(ViewActions.typeText("12"));
-        onView(withId(R.id.item_month_edit_text)).perform(ViewActions.typeText("06"));
-        onView(withId(R.id.item_year_edit_text)).perform(ViewActions.typeText("2023"));
+        onView(withText("MM/DD/YYYY")).perform(click());
+        onView(withText("OK")).perform(click());
         onView(withId(R.id.input_tag_edit_text)).perform(ViewActions.typeText("My Tag"));
         onView(withId(R.id.add_tags_button)).perform(click());
         onView(withId(R.id.price_edit_text)).perform(ViewActions.typeText("1234.56"));
         onView(withId(R.id.comments_edit_text)).perform(ViewActions.typeText("Some comment"));
+        onView(withText("OK")).perform(click());
 
-        //TODO: dialog button has no id, so I cannot exit the dialog properly
-        onView(withId(R.id.view_item_button)).perform(click());
-        //TODO: dialog button has no id, so I cannot click on the delete button properly
-        onView(withText("House")).check(doesNotExist());
-    }
-    public void testInvalidItem() {
-        Intents.init();
-        onView(withId(R.id.emailEntry))
-                .perform(ViewActions.typeText("admin@admin.com"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.passwordEntry))
-                .perform(ViewActions.typeText("password"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.signInButton)).perform(click());
-        try{
-            // Espresso is so bad that without this sleep the test fails, even though visually
-            // it still works
-            Thread.sleep(3000);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        intended(hasComponent(MainActivity.class.getName()));
-        Intents.release();
         onView(withId(R.id.add_item_button)).perform(click());
-        onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House"));
+        onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House2"));
+        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("House2"));
+        onView(withId(R.id.serial_edit_text)).perform(ViewActions.typeText("12345"));
+        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("House Model"));
+        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("House Make"));
+        onView(withText("MM/DD/YYYY")).perform(click());
+        onView(withText("OK")).perform(click());
+        onView(withId(R.id.input_tag_edit_text)).perform(ViewActions.typeText("My Tag"));
+        onView(withId(R.id.add_tags_button)).perform(click());
+        onView(withId(R.id.price_edit_text)).perform(ViewActions.typeText("1234.56"));
         onView(withId(R.id.comments_edit_text)).perform(ViewActions.typeText("Some comment"));
+        onView(withText("OK")).perform(click());
 
-        //TODO: dialog button has no id, so I cannot exit the dialog properly
-        onView(withText("Invalid Input")).check(matches(isDisplayed()));
+        onView(withId(R.id.filter_items_button)).perform(click());
+        onView(withId(R.id.filter_keywords_edit_text)).perform(ViewActions.typeText("House2"));
+        onView(withId(R.id.add_keyword_button)).perform(click());
+        onView(withText("Apply")).perform(click());
+        onView(withText("House2")).check(matches(isDisplayed()));
+
+
     }
+//    @Test
+//    public void testViewItem() {
+//        onView(withId(R.id.add_item_button)).perform(click());
+//        onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House"));
+//        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("My House"));
+//        onView(withId(R.id.serial_edit_text)).perform(ViewActions.typeText("12345"));
+//        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("House Model"));
+//        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("House Make"));
+//        onView(withText("MM/DD/YYYY")).perform(click());
+//        onView(withText("OK")).perform(click());
+//        onView(withId(R.id.input_tag_edit_text)).perform(ViewActions.typeText("My Tag"));
+//        onView(withId(R.id.add_tags_button)).perform(click());
+//        onView(withId(R.id.price_edit_text)).perform(ViewActions.typeText("1234.56"));
+//        onView(withId(R.id.comments_edit_text)).perform(ViewActions.typeText("Some comment"));
+//        onView(withText("OK")).perform(click());
+//        onView(withId(R.id.view_item_button)).perform(click());
+//    }
+//    public void testDeleteItem() {
+//        Intents.init();
+//        onView(withId(R.id.emailEntry))
+//                .perform(ViewActions.typeText("admin@admin.com"));
+//        Espresso.closeSoftKeyboard();
+//        onView(withId(R.id.passwordEntry))
+//                .perform(ViewActions.typeText("password"));
+//        Espresso.closeSoftKeyboard();
+//        onView(withId(R.id.signInButton)).perform(click());
+//        try{
+//            // Espresso is so bad that without this sleep the test fails, even though visually
+//            // it still works
+//            Thread.sleep(3000);
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        intended(hasComponent(MainActivity.class.getName()));
+//        Intents.release();
+//        onView(withId(R.id.add_item_button)).perform(click());
+//        onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House"));
+//        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("My House"));
+//        onView(withId(R.id.serial_edit_text)).perform(ViewActions.typeText("12345"));
+//        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("House Model"));
+//        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("House Make"));
+//        onView(withId(R.id.item_day_edit_text)).perform(ViewActions.typeText("12"));
+//        onView(withId(R.id.item_month_edit_text)).perform(ViewActions.typeText("06"));
+//        onView(withId(R.id.item_year_edit_text)).perform(ViewActions.typeText("2023"));
+//        onView(withId(R.id.input_tag_edit_text)).perform(ViewActions.typeText("My Tag"));
+//        onView(withId(R.id.add_tags_button)).perform(click());
+//        onView(withId(R.id.price_edit_text)).perform(ViewActions.typeText("1234.56"));
+//        onView(withId(R.id.comments_edit_text)).perform(ViewActions.typeText("Some comment"));
+//
+//        //TODO: dialog button has no id, so I cannot exit the dialog properly
+//        onView(withId(R.id.view_item_button)).perform(click());
+//        //TODO: dialog button has no id, so I cannot click on the delete button properly
+//        onView(withText("House")).check(doesNotExist());
+//    }
+//    public void testInvalidItem() {
+//        Intents.init();
+//        onView(withId(R.id.emailEntry))
+//                .perform(ViewActions.typeText("admin@admin.com"));
+//        Espresso.closeSoftKeyboard();
+//        onView(withId(R.id.passwordEntry))
+//                .perform(ViewActions.typeText("password"));
+//        Espresso.closeSoftKeyboard();
+//        onView(withId(R.id.signInButton)).perform(click());
+//        try{
+//            // Espresso is so bad that without this sleep the test fails, even though visually
+//            // it still works
+//            Thread.sleep(3000);
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        intended(hasComponent(MainActivity.class.getName()));
+//        Intents.release();
+//        onView(withId(R.id.add_item_button)).perform(click());
+//        onView(withId(R.id.name_edit_text)).perform(ViewActions.typeText("House"));
+//        onView(withId(R.id.comments_edit_text)).perform(ViewActions.typeText("Some comment"));
+//
+//        //TODO: dialog button has no id, so I cannot exit the dialog properly
+//        onView(withText("Invalid Input")).check(matches(isDisplayed()));
+//    }
 }
